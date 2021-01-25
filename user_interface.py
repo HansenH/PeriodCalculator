76c9387d52f65e75ec11127926b68359a74f486d
honghansen <honghansen@bytedance.com>
2021-01-25 21:29:04 +0800

    u

--- user_interface.py
+++ user_interface.py
@@ -1,6 +1,7 @@
 # -*- coding:utf-8 -*-
 from tkinter import Tk
-from tkinter.ttk import Button  # win本地化主题模块，元件更美观
+from tkinter.ttk import Button  # ttk: win本地化主题模块，组件更美观
+from tkinter import Frame
 import ctypes
 
 class UserInterface():
@@ -11,24 +12,45 @@ class UserInterface():
         self.window = Tk()
         self.window.iconbitmap('icon.ico')
         self.window.title('Period Calculator V%s' % self.main.version)
-        self.window.geometry('{}x{}'.format(self.main.window_width, self.main.window_height))
+        self.scale_factor = 1   #默认缩放因子
         self.dpi_adapt()    #高DPI适配，删除此行可能导致字体和组件边缘模糊
-        self.test()
+        self.window_width = self.main.window_width * self.scale_factor
+        self.window_height = self.main.window_height * self.scale_factor
+        self.window.geometry('{}x{}'.format(self.window_width, self.window_height)) #窗口大小
+        self.window.resizable(0,0)      #锁定窗口大小
+        self.make_frame_left()
+        self.make_frame2()
 
     def dpi_adapt(self):
         '''解决高分屏下程序界面模糊问题（高DPI适配）'''
         #设置由应用程序自己控制缩放
         ctypes.windll.shcore.SetProcessDpiAwareness(1)
         #获得显示设置的缩放因子
-        scale_factor = int(ctypes.windll.shcore.GetScaleFactorForDevice(0) / 100)
-        #设置缩放
-        self.window.tk.call('tk', 'scaling', scale_factor * 1.5)
-        self.window.geometry('{}x{}'.format(self.main.window_width * scale_factor, 
-                                        self.main.window_height * scale_factor))
+        self.scale_factor = int(ctypes.windll.shcore.GetScaleFactorForDevice(0) / 100)
+        #设置组件缩放
+        self.window.tk.call('tk', 'scaling', self.scale_factor * 1.6)
 
-    def test(self):
-        btn = Button(self.window, text="周期")
-        btn.pack()
+
+    class FrameLeft():
+        '''左边栏框架'''
+        def __init__(self, ui)
+
+
+    def make_frame_left(self):
+        self.frame_left = Frame(self.window, bg='#FFC0CB', bd=10, relief='groove', width=self.window_width * 0.3, height=self.window_height)
+        self.frame_left.pack(side='left')
+        self.frame_left.pack_propagate(0)
+        btn = Button(self.frame_left, text='HHHH')
+        btn.place(x=50, y=100, anchor='nw')
+        
+
+    def make_frame2(self):
+        self.frame2 = Frame(self.window, bg='#FFF0F5', bd=10, relief='groove', width=self.window_width * 0.7, height=self.window_height)
+        self.frame2.pack(side='right')
+        self.frame2.pack_propagate(0)
+
+
+    
 
 
 
